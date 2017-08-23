9418073392bd5a04799fb3bce091db407732eec4
Rafael Rodriguez <rafael@slidebean.com>
2017-08-22 22:57:03 -0600

    Add scrolling

--- mia/js/app.js
+++ mia/js/app.js
@@ -11,8 +11,11 @@ function enterAsClick() {
 function publish() {
   var output = document.getElementById("output");
   var input = document.getElementById("input");
-  output.innerHTML = output.innerHTML + "<p><span>You: " + input.value + "</span></p>";
-  input.value = "";
+  if (input.value.length > 0) {
+    output.innerHTML = output.innerHTML + "<p><span>You: " + input.value + "</span></p>";
+    input.value = "";
+  }
+  window.scrollTo(0, document.body.scrollHeight);
 }
 
 window.onload = enterAsClick();
