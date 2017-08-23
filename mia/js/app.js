749a8f341c8de6843cd53507002f30e4c686a8f7
Rafael Rodriguez <rafael@slidebean.com>
2017-08-22 22:22:33 -0600

    Adding publish method

--- mia/js/app.js
+++ mia/js/app.js
@@ -5,11 +5,11 @@
       document.getElementById("button").click();
     }
   });
-
-  function publish() {
-    var output = document.getElementById("output");
-    var input = document.getElementById("input");
-    output.innerHTML = "<p><span>" + input.value + "</span></p>" + output.innerHTML;
-    input.value = "";
-  }
 })();
+
+function publish() {
+  var output = document.getElementById("output");
+  var input = document.getElementById("input");
+  output.innerHTML = "<p><span>" + input.value + "</span></p>" + output.innerHTML;
+  input.value = "";
+}
