7d3518f66a923e68948ab340c923c9e4bad0c7fa
Rafael Rodriguez <rafael@slidebean.com>
2017-08-22 21:47:35 -0600

    Adding styles and features to Mia

--- mia/js/app.js
+++ mia/js/app.js
@@ -1,25 +1,46 @@
 (function() {
 
-    var output = PUBNUB.$('output'), 
-        input = PUBNUB.$('input'), 
-        button = PUBNUB.$('button'),
-        avatar = PUBNUB.$('avatar'),
-        presence = PUBNUB.$('presence');
+     var output = PUBNUB.$('output'), 
+         input = PUBNUB.$('input'), 
+         button = PUBNUB.$('button'),
+         avatar = PUBNUB.$('avatar'),
+         miaAvatar = PUBNUB.$('avatar'),
+         presence = PUBNUB.$('presence');
 
     var channel = 'mchat';
     
     // Assign a random avatar in random color
     avatar.className = 'face-' + ((Math.random() * 13 + 1) >>> 0) + ' color-' + ((Math.random() * 10 + 1) >>> 0);
+    miaAvatar.className = 'face-' + ((Math.random() * 13 + 1) >>> 0) + ' color-' + ((Math.random() * 10 + 1) >>> 0);
 
     var p = PUBNUB.init({
         subscribe_key: 'sub-c-f762fb78-2724-11e4-a4df-02ee2ddab7fe',
         publish_key:   'pub-c-156a6d5f-22bd-4a13-848d-b5b4d4b36695'
     });
+    
+    var miaP = PUBNUB.init({
+        subscribe_key: 'sub-c-f762fb78-2724-11e4-a4df-02ee2ddab7ff',
+        publish_key:   'pub-c-156a6d5f-22bd-4a13-848d-b5b4d4b36696'
+    });
 
     p.subscribe({
         channel  : channel,
         callback : function(m) { 
-            output.innerHTML = '<p><i class="' + m.avatar + '"></i><span>' +  m.text.replace( /[<>]/ig, '' ) + '</span></p>' + output.innerHTML; 
+            output.innerHTML = '<p><i class="' + m.avatar + '"></i><span>' +  m.text.replace( /[<>]/ig, '' ) + '</span></p>' + output.innerHTML;
+        },
+        presence: function(m){
+            if(m.occupancy > 1) {
+                presence.textContent = m.occupancy + ' people online';
+            } else {
+                presence.textContent = 'Nobody else is online';
+            }
+        }
+    });
+    
+    miaP.subscribe({
+        channel  : channel,
+        callback : function(m) { 
+            output.innerHTML = '<p><i class="' + m.avatar + '"></i><span>' +  m.text.replace( /[<>]/ig, '' ) + '</span></p>' + output.innerHTML;
         },
         presence: function(m){
             if(m.occupancy > 1) {
@@ -33,14 +54,25 @@
     p.bind('keyup', input, function(e) {
         (e.keyCode || e.charCode) === 13 && publish()
     });
+    
+    miaP.bind('keyup', input, function(e) {
+        (e.keyCode || e.charCode) === 13 && publish()
+    });
 
     p.bind('click', button, publish);
+    
+    miaP.bind('click', button, publish);
 
     function publish() {
         p.publish({
             channel : channel, 
             message : {avatar: avatar.className, text: input.value}, 
             x : (input.value='')
+        });
+        
+        miaP.publish({
+            channel : channel, 
+            message : {avatar: avatar.className, text: "Hello"}
         });
     }
 
